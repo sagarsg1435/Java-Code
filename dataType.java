d0ea628e6141772afcab84dc8e6380cdbd4f27bb
Sagar Gawade <89600313+sagarsg1435@users.noreply.github.com>
2022-03-23 18:34:24 +0530

    Add files via upload

---
+++ dataType.java
@@ -0,0 +1,14 @@
+
+public class dataType {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+float a = 1.3f;
+float b = 2.3f;
+float c = a+b;
+float d = a-b;
+System.out.println(c);
+System.out.println(d);
+	}
+
+}
