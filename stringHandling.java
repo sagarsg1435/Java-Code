aac7fe8abd42ca24237121f9f550759347ed0d8d
Sagar Gawade <89600313+sagarsg1435@users.noreply.github.com>
2022-03-23 18:31:11 +0530

    Add files via upload

---
+++ stringHandling.java
@@ -0,0 +1,11 @@
+
+public class stringHandling {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+String A = "Hey Hi Everyone!";
+String B = "My Name is Sagar";
+System.out.println(A+B);
+	}
+
+}
