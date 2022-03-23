f14dde9269aa1a23934ae3a7162698aced08192f
Sagar Gawade <89600313+sagarsg1435@users.noreply.github.com>
2022-03-23 18:29:20 +0530

    Add files via upload

---
+++ gettingTwoNo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class gettingTwoNo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Enter First Number");
+Scanner sc = new Scanner(System.in);
+int a = sc.nextInt();
+System.out.println("Enter Second Number");
+int b = sc.nextInt();
+int c = a+b;
+System.out.println(c);
+	}
+
+}
